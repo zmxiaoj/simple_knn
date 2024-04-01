96b084880aa459b91d7866d6f8d20a204dd5f4ca
zmxiaoj <935834551@qq.com>
2024-04-01 11:06:40 +0800

    Add comments

--- simple_knn.cu
+++ simple_knn.cu
@@ -42,17 +42,25 @@ struct CustomMax
 	}
 };
 
+// 将32位整数x转换为morton码
 __host__ __device__ uint32_t prepMorton(uint32_t x)
 {
+	// 将x每个字节的最低2位复制到最高2位
 	x = (x | (x << 16)) & 0x030000FF;
+	// 将x每个字节的最低4位复制到最高4位
 	x = (x | (x << 8)) & 0x0300F00F;
+	// 将x每个字节的最低8位复制到最高8位
 	x = (x | (x << 4)) & 0x030C30C3;
+	// 将x每个字节的最低16位复制到最高16位
 	x = (x | (x << 2)) & 0x09249249;
 	return x;
 }
 
+// 既可以在host上调用，也可以在device上调用
 __host__ __device__ uint32_t coord2Morton(float3 coord, float3 minn, float3 maxx)
 {
+	// 将坐标映射到[0, 2^10 - 1]区间
+	// 计算每个维度的morton码
 	uint32_t x = prepMorton(((coord.x - minn.x) / (maxx.x - minn.x)) * ((1 << 10) - 1));
 	uint32_t y = prepMorton(((coord.y - minn.y) / (maxx.y - minn.y)) * ((1 << 10) - 1));
 	uint32_t z = prepMorton(((coord.z - minn.z) / (maxx.z - minn.z)) * ((1 << 10) - 1));
@@ -62,10 +70,12 @@ __host__ __device__ uint32_t coord2Morton(float3 coord, float3 minn, float3 maxx
 
 __global__ void coord2Morton(int P, const float3* points, float3 minn, float3 maxx, uint32_t* codes)
 {
+	// 每个thread对应一个点
 	auto idx = cg::this_grid().thread_rank();
 	if (idx >= P)
 		return;
 
+	// 计算每个点的morton码
 	codes[idx] = coord2Morton(points[idx], minn, maxx);
 }
 
@@ -77,28 +87,36 @@ struct MinMax
 
 __global__ void boxMinMax(uint32_t P, float3* points, uint32_t* indices, MinMax* boxes)
 {
+	// thread的idx
 	auto idx = cg::this_grid().thread_rank();
 
 	MinMax me;
+	// 如果idx小于P，则me的minn和maxx初始化为当前点的坐标
 	if (idx < P)
 	{
 		me.minn = points[indices[idx]];
 		me.maxx = points[indices[idx]];
 	}
+	// 如果idx大于P，则me的minn和maxx初始化为FLT_MAX和-FLT_MAX
+	// FLT_MAX表示float的最大值，-FLT_MAX表示float的最小值
 	else
 	{
 		me.minn = { FLT_MAX, FLT_MAX, FLT_MAX };
 		me.maxx = { -FLT_MAX,-FLT_MAX,-FLT_MAX };
 	}
 
+	// 创建BOX_SIZE大小的共享内存，用于存储block内每个thread的MinMax
 	__shared__ MinMax redResult[BOX_SIZE];
 
+	// 并行归并
 	for (int off = BOX_SIZE / 2; off >= 1; off /= 2)
 	{
+		// 当前thread的idx 小于 off，则将当前thread的MinMax保存到共享内存中
 		if (threadIdx.x < 2 * off)
 			redResult[threadIdx.x] = me;
+		// block内thread同步
 		__syncthreads();
-
+		// 当前thread的idx 小于 off，则将当前thread的MinMax和off后的thread的MinMax进行归并
 		if (threadIdx.x < off)
 		{
 			MinMax other = redResult[threadIdx.x + off];
@@ -109,10 +127,12 @@ __global__ void boxMinMax(uint32_t P, float3* points, uint32_t* indices, MinMax*
 			me.maxx.y = max(me.maxx.y, other.maxx.y);
 			me.maxx.z = max(me.maxx.z, other.maxx.z);
 		}
+		// block内thread同步
 		__syncthreads();
 	}
-
+	// 最后idx为0的thread中保存了block内归并的结果
 	if (threadIdx.x == 0)
+		// 将block对应的结果保存到boxes中
 		boxes[blockIdx.x] = me;
 }
 
@@ -131,8 +151,12 @@ __device__ __host__ float distBoxPoint(const MinMax& box, const float3& p)
 template<int K>
 __device__ void updateKBest(const float3& ref, const float3& point, float* knn)
 {
+	// 计算当前点point和参考点ref的距离
 	float3 d = { point.x - ref.x, point.y - ref.y, point.z - ref.z };
+	// 计算距离的平方
 	float dist = d.x * d.x + d.y * d.y + d.z * d.z;
+	// 如果当前点的距离小于knn中的最大值，则更新knn
+	// 将当前点的距离插入到knn中
 	for (int j = 0; j < K; j++)
 	{
 		if (knn[j] > dist)
@@ -146,76 +170,117 @@ __device__ void updateKBest(const float3& ref, const float3& point, float* knn)
 
 __global__ void boxMeanDist(uint32_t P, float3* points, uint32_t* indices, MinMax* boxes, float* dists)
 {
+	// 每个thread对应一个点
 	int idx = cg::this_grid().thread_rank();
 	if (idx >= P)
 		return;
-
+	
+	// 取出当前点的坐标
 	float3 point = points[indices[idx]];
 	float best[3] = { FLT_MAX, FLT_MAX, FLT_MAX };
 
+	// 遍历当前点的前后各3个点
 	for (int i = max(0, idx - 3); i <= min(P - 1, idx + 3); i++)
 	{
 		if (i == idx)
 			continue;
+		// 更新关于当前点point最近的3个点的距离平方
 		updateKBest<3>(point, points[indices[i]], best);
 	}
-
+	// 记录当前点的最近3个点距离平方的最大值
 	float reject = best[2];
 	best[0] = FLT_MAX;
 	best[1] = FLT_MAX;
 	best[2] = FLT_MAX;
 
+	// 遍历boxes中的每个box，找到最近的box，再找到最近的3个点
 	for (int b = 0; b < (P + BOX_SIZE - 1) / BOX_SIZE; b++)
 	{
 		MinMax box = boxes[b];
+		// 计算box和当前点的距离
 		float dist = distBoxPoint(box, point);
+		// 如果box和当前点的距离大于reject或者大于best[2]，则跳过
 		if (dist > reject || dist > best[2])
 			continue;
-
+		// 遍历box中的每个点
 		for (int i = b * BOX_SIZE; i < min(P, (b + 1) * BOX_SIZE); i++)
 		{
 			if (i == idx)
 				continue;
+			// 更新关于当前点point最近的3个点的距离平方
 			updateKBest<3>(point, points[indices[i]], best);
 		}
 	}
+	// 计算当前点的3个最近点距离平方的均值
 	dists[indices[idx]] = (best[0] + best[1] + best[2]) / 3.0f;
 }
 
+/**
+ * @brief host函数
+ * 
+ * @param P [in] int, 输入点的数量
+ * @param points [in] tensor, 输入点的坐标
+ * @param meanDists [out] tensor, 输出点的均值距离
+ */
 void SimpleKNN::knn(int P, float3* points, float* meanDists)
 {
 	float3* result;
+	// 在GPU上分配内存，存储float3数据
 	cudaMalloc(&result, sizeof(float3));
 	size_t temp_storage_bytes;
 
 	float3 init = { 0, 0, 0 }, minn, maxx;
 
+	// 计算cub的归约函数需要的临时内存大小
 	cub::DeviceReduce::Reduce(nullptr, temp_storage_bytes, points, result, P, CustomMin(), init);
+	// 分配内存
 	thrust::device_vector<char> temp_storage(temp_storage_bytes);
 
+	// 实际调用cub的归约函数
+	// 通过cub的归约函数计算points中每个维度的最小值，保存在result中
 	cub::DeviceReduce::Reduce(temp_storage.data().get(), temp_storage_bytes, points, result, P, CustomMin(), init);
+	// 将最小值从GPU拷贝到CPU
 	cudaMemcpy(&minn, result, sizeof(float3), cudaMemcpyDeviceToHost);
 
+	// 通过cub的归约函数计算points中每个维度的最大值，保存在result中
 	cub::DeviceReduce::Reduce(temp_storage.data().get(), temp_storage_bytes, points, result, P, CustomMax(), init);
+	// 将最大值从GPU拷贝到CPU
 	cudaMemcpy(&maxx, result, sizeof(float3), cudaMemcpyDeviceToHost);
 
+	// 设备向量device_vector，在GPU内存中分配，可以直接在cuda核函数中使用
+	// 分配P个uint32_t的内存，用于存储morton码
 	thrust::device_vector<uint32_t> morton(P);
+	// 分配P个uint32_t的内存，用于存储排序后的morton码
 	thrust::device_vector<uint32_t> morton_sorted(P);
+	// 调用kernel函数，计算每个点的morton码
 	coord2Morton << <(P + 255) / 256, 256 >> > (P, points, minn, maxx, morton.data().get());
 
+	// 创建P大小的uint32_t向量，用于存储点的idx
 	thrust::device_vector<uint32_t> indices(P);
+	// 初始化indices为[0, 1, 2, ..., P-1]
 	thrust::sequence(indices.begin(), indices.end());
+	// 创建P大小的uint32_t向量，用于存储排序后的点的idx
 	thrust::device_vector<uint32_t> indices_sorted(P);
 
+	// 计算cub的排序函数需要的临时内存大小
 	cub::DeviceRadixSort::SortPairs(nullptr, temp_storage_bytes, morton.data().get(), morton_sorted.data().get(), indices.data().get(), indices_sorted.data().get(), P);
 	temp_storage.resize(temp_storage_bytes);
 
+	// 实际调用cub的基数排序函数
+	// key - morton码，value - indices
+	// 将morton码和indices按照morton码排序
 	cub::DeviceRadixSort::SortPairs(temp_storage.data().get(), temp_storage_bytes, morton.data().get(), morton_sorted.data().get(), indices.data().get(), indices_sorted.data().get(), P);
 
+	// 初始化boxes数目
 	uint32_t num_boxes = (P + BOX_SIZE - 1) / BOX_SIZE;
+	// 创建num_boxes大小的MinMax向量，用于存储每个box的minn和maxx
 	thrust::device_vector<MinMax> boxes(num_boxes);
+	// 调用kernel函数，计算每个box的minn和maxx
+	// grid大小为num_boxes，block大小为BOX_SIZE(1024)
 	boxMinMax << <num_boxes, BOX_SIZE >> > (P, points, indices_sorted.data().get(), boxes.data().get());
+	// 调用kernel函数，计算每个点最近的3个点的距离平方的均值
 	boxMeanDist << <num_boxes, BOX_SIZE >> > (P, points, indices_sorted.data().get(), boxes.data().get(), meanDists);
 
+	// 释放GPU内存
 	cudaFree(result);
 }
--- spatial.cu
+++ spatial.cu
@@ -15,11 +15,15 @@
 torch::Tensor
 distCUDA2(const torch::Tensor& points)
 {
+  // 输入点的数目
   const int P = points.size(0);
 
   auto float_opts = points.options().dtype(torch::kFloat32);
+  // 初始化大小为P的全0张量
   torch::Tensor means = torch::full({P}, 0.0, float_opts);
-  
+  // 调用SimpleKNN::knn函数，计算每个点到其他点的距离
+  // 输入点的数目、点的坐标tensor，输出的means张量
+  // means保存每个点到最近3个点的距离平方的均值
   SimpleKNN::knn(P, (float3*)points.contiguous().data<float>(), means.contiguous().data<float>());
 
   return means;
